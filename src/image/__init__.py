80287465b77c592f8ec29e7c4d8d5cac16ce1f07
guochen <chenguo@jieyuechina.com>
2019-10-22 16:04:36 +0800

    find logo in image

--- src/image/__init__.py
+++ src/image/__init__.py
@@ -6,9 +6,9 @@ import os
 import sys
 sys.path.append(os.path.abspath('..'))
 
-from aip import AipNlp
+from aip import AipImageClassify
 
-from base.utils import img2base64, access_token
+from base.utils import img2base64, access_token, get_img_content
 from base.utils import get_json, post_json
 
 import json
@@ -16,14 +16,18 @@ import json
 class Image(object):
 
     def __init__(self, filename, regs):
-        self.app_id, self.app_key, self.secret_key = regs
+        app_id, app_key, secret_key = regs
+#        self.app_id, self.app_key, self.secret_key = app_id, app_key, secret_key
         self.filename = filename
-        self.base64 = img2base64(filename)
+        self.img = get_img_content(filename)
+        self.base64 = img2base64(self.img)
+        self.client = AipImageClassify(app_id, app_key, secret_key)
 
     def is_same_as(self, image):
         return self.base64 == image.base64
 
-    def find_logo(self):
+    '''
+    def _find_logo(self):
         res = None
         token = access_token(url='https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=' + self.app_key + '&client_secret=' + self.secret_key,
             key='access_token')
@@ -35,6 +39,14 @@ class Image(object):
         if data is not None:
             res = data.get('result')
         return res
+    '''
+
+    def find_logo(self, options=None):
+        if options is None:
+            options = {}
+        return self.client.logoSearch(self.img, options)
+
+
 
 if '__main__' == __name__:
     from config import IMAGE_REGS
